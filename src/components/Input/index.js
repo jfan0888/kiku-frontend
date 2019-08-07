532766f5c51afd7a55260033a5047211e4c33675
jfan0888 <bsrg5111@gmail.com>
2019-08-07 18:30:36 +0100

    updated custominput with custom styling

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,11 +3,32 @@ import PropTypes from 'prop-types';
 
 import './styles.scss';
 
-const CustomInput = ({ className, type, placeholder, onChange }) => (
-  <div className={`custom-input-container ${className}`}>
-    <input type={type} placeholder={placeholder} onChange={onChange} />
-  </div>
-);
+const emailIcon = require('../../assets/images/email.png');
+const keyIcon = require('../../assets/images/key.png');
+
+const PlaceholderImage = ({ type }) => {
+  switch (type) {
+    case 'email':
+      return <img className="placeholder-image" src={emailIcon} />;
+    case 'password':
+      return <img className="placeholder-image" src={keyIcon} />;
+    default:
+      return null;
+  }
+};
+
+class CustomInput extends React.Component {
+  render() {
+    const { className, type, placeholder, onChange } = this.props;
+
+    return (
+      <div className={`custom-input-container ${className}`}>
+        <PlaceholderImage type={type} />
+        <input type={type} placeholder={placeholder} onChange={onChange} />
+      </div>
+    );
+  }
+}
 
 CustomInput.propTypes = {
   type: PropTypes.string,
