3800998add7834930b152a7dc6c3f484c53fda14
jfan0888 <bsrg5111@gmail.com>
2019-08-07 11:20:56 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hello React,Webpack 4 & Babel 8!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
