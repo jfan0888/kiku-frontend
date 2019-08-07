f0650f0e13cc306084612e398c73ea09434d6fd5
jfan0888 <bsrg5111@gmail.com>
2019-08-07 15:19:05 +0100

    updated the initial app config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>Hello React,Webpack 4 & Babel 8!</div>;
-};
+import { App } from './components';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
