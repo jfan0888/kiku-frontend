c7e95139f26737023ae3406e9ab487644a5b9b3d
jfan0888 <bsrg5111@gmail.com>
2019-08-07 18:32:32 +0100

    completed home ui

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { CustomInput } from '../../components';
+import { CustomInput, CustomButton } from '../../components';
 import './styles.scss';
 
 const kikuLogo = require('../../assets/images/kiku-logo.png');
@@ -18,17 +19,43 @@ class Home extends React.Component {
           <div className="page-title">Music that Moves</div>
         </div>
         <div className="page-content">
-          <CustomInput
-            className="contact-input-item"
-            type="text"
-            placeholder="First Name"
-          />
-          <CustomInput
-            className="contact-input-item"
-            type="text"
-            placeholder="Last Name"
-          />
+          <div className="input-form-wrapper">
+            <CustomInput
+              className="home-input-item"
+              type="text"
+              placeholder="First Name"
+            />
+            <CustomInput
+              className="home-input-item"
+              type="text"
+              placeholder="Last Name"
+            />
+            <CustomInput
+              className="home-input-item"
+              type="email"
+              placeholder="Email"
+            />
+            <CustomInput
+              className="home-input-item"
+              type="password"
+              placeholder="Password"
+            />
+            <CustomInput
+              className="home-input-item"
+              type="password"
+              placeholder="Confirm Password"
+            />
+            <CustomButton title="create account" />
+          </div>
         </div>
+        <footer>
+          <div className="footer-text">
+            Already Have An Account?
+            <Link className="text-link" to="/login">
+              Login
+            </Link>
+          </div>
+        </footer>
       </div>
     );
   }
