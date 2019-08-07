83811479699b5d9b6352536a7ccc850cf6dc15e1
jfan0888 <bsrg5111@gmail.com>
2019-08-07 17:00:48 +0100

    reconfiguration of the root files

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 
-const App = () => <div>app</div>;
+import Home from '../containers/Home';
+
+const history = createBrowserHistory();
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './styles/app.scss';
+
 import { App } from './components';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
