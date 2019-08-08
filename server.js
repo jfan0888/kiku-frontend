634e101d3dcff9eaf9ea51046cd10a84b7167216
jfan0888 <bsrg5111@gmail.com>
2019-08-08 18:57:41 +0100

    resolved heroku app refresh issue

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ const path = require('path');
 const port = process.env.PORT || 3001;
 
 app.use('/', express.static(path.join(__dirname, 'dist')));
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(port, () => console.log('Listening on Port', port));
