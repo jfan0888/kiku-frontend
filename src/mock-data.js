abdec43690ebdd3f0de3a3e3c32ec353cdfa3cc4
jfan0888 <bsrg5111@gmail.com>
2019-08-08 18:38:59 +0100

    added mock data

---
+++ src/mock-data.js
@@ -0,0 +1,47 @@
+const ideasData = [
+  {
+    id: 1,
+    title: 'idea 01',
+  },
+  {
+    id: 2,
+    title: 'idea 02',
+  },
+  {
+    id: 3,
+    title: 'idea 03',
+  },
+  {
+    id: 4,
+    title: 'idea 04',
+  },
+  {
+    id: 5,
+    title: 'idea 05',
+  },
+];
+
+const songsData = [
+  {
+    id: 1,
+    title: 'kiku jingle',
+  },
+  {
+    id: 2,
+    title: 'song 02',
+  },
+  {
+    id: 3,
+    title: 'song 03',
+  },
+  {
+    id: 4,
+    title: 'song 04',
+  },
+  {
+    id: 5,
+    title: 'song 05',
+  },
+];
+
+export { ideasData, songsData };
