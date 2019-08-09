8f86a016753e09b39f42ea9143177d08bb674564
jfan0888 <bsrg5111@gmail.com>
2019-08-10 00:15:54 +0100

    updated song item view

--- src/containers/Song/index.js
+++ src/containers/Song/index.js
@@ -1,17 +1,77 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-import { GeneralView } from '../../components';
+import { GeneralView, AddableList, ProgressButton } from '../../components';
+import './styles.scss';
 
 class Song extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      btnText: '',
+    };
   }
+
+  componentDidMount() {
+    this.updateStatus(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.updateStatus(nextProps);
+  }
+
+  updateStatus = props => {
+    const { location } = props.history;
+    const { statusText } = location.state;
+
+    switch (statusText) {
+      case 'idea':
+        this.setState({ btnText: 'in progress' });
+        break;
+      case 'in progress':
+        this.setState({ btnText: 'complete' });
+        break;
+      case 'completed':
+        this.setState({ btnText: 'share' });
+        break;
+      default:
+        break;
+    }
+  };
+
+  onClickMove = () => {
+    const { history } = this.props;
+    const { location } = history;
+    const { statusText, item } = location.state;
+
+    switch (statusText) {
+      case 'idea':
+        history.push(`/in-progress/${item.id}`, {
+          item,
+          statusText: 'in progress',
+        });
+        break;
+      case 'in progress':
+        history.push(`/completed/${item.id}`, {
+          item,
+          statusText: 'completed',
+        });
+        break;
+      case 'complete':
+        history.push(`/completed/${item.id}`, {
+          item,
+          statusText: 'completed',
+        });
+        break;
+      default:
+        break;
+    }
+  };
 
   render() {
     const { location } = this.props.history;
     const { statusText, item, hasMusic } = location.state;
+    const { btnText } = this.state;
 
     return (
       <GeneralView
@@ -19,7 +79,16 @@ class Song extends React.Component {
         statusText={statusText}
         hasMusic={hasMusic}
       >
-        <div className="page-content">Song</div>
+        <AddableList title="archive" />
+        <AddableList title="lyrics" />
+        <div className="lyrics-list">Add Some Lyrics</div>
+        <AddableList title="add a co-writer" />
+        <div className="progress-button-wrapper">
+          <ProgressButton
+            title={`move to ${btnText}`}
+            nextStepHandler={this.onClickMove}
+          />
+        </div>
       </GeneralView>
     );
   }
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,8 +1,8 @@
-import Home from './Home';
+import SignIn from './SignIn';
 import Profile from './Profile';
 import Ideas from './Ideas';
 import InProgress from './InProgress';
 import Complete from './Complete';
 import Song from './Song';
 
-export { Home, Profile, Ideas, InProgress, Complete, Song };
+export { SignIn, Profile, Ideas, InProgress, Complete, Song };
