163b6d373343e1a256a400dab151272e8a94f6a3
jfan0888 <bsrg5111@gmail.com>
2019-08-10 00:15:39 +0100

    updated root files

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,14 @@ import CustomInput from './Input';
 import CustomButton from './Button';
 import GeneralView from './Logged';
 import ObjectButton from './ObjectButton';
+import AddableList from './AddableList';
+import ProgressButton from './Button/ProgressButton';
 
-export { CustomInput, CustomButton, GeneralView, ObjectButton };
+export {
+  CustomInput,
+  CustomButton,
+  GeneralView,
+  ObjectButton,
+  AddableList,
+  ProgressButton,
+};
