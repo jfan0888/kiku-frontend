6215ac98966e064280262e66e6eeee9e207462c1
jfan0888 <bsrg5111@gmail.com>
2019-08-12 18:34:12 +0100

    added font loader into the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,18 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader'],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
   },
   devServer: {
