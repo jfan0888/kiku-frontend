943f26bdaba921a527441fcc04757e557429f1b1
jfan0888 <bsrg5111@gmail.com>
2019-08-14 03:09:45 +0100

    configuration of the song

---
+++ src/reducers/song.js
@@ -0,0 +1,51 @@
+import {
+  GET_SONG_REQUEST,
+  GET_SONG_SUCCESS,
+  GET_SONG_ERROR,
+  SET_SONG_REQUEST,
+  SET_SONG_SUCCESS,
+  SET_SONG_ERROR,
+} from '../constants/actionTypes';
+
+const initialState = {
+  isLoading: false,
+  error: null,
+  data: {
+    id: 1,
+    archive: [],
+    lyrics: [],
+    coWriters: [],
+    status: null,
+  },
+};
+
+export default (state = initialState, { type, data, error }) => {
+  switch (type) {
+    case GET_SONG_REQUEST:
+    case SET_SONG_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        error: null,
+      };
+
+    case GET_SONG_SUCCESS:
+    case SET_SONG_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        data,
+      };
+
+    case GET_SONG_ERROR:
+    case SET_SONG_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error,
+      };
+
+    default:
+      return state;
+  }
+};
