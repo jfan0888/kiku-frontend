4d0aee672bba2916ce7287ec4790f01dc364c474
jfan0888 <bsrg5111@gmail.com>
2019-08-14 03:08:26 +0100

    updated progress button

--- src/components/Button/ProgressButton.js
+++ src/components/Button/ProgressButton.js
@@ -3,20 +3,31 @@ import PropTypes from 'prop-types';
 
 import './styles.scss';
 const forwardIcon = require('../../assets/images/forward.png');
+const backwardIcon = require('../../assets/images/backward.png');
 
-const ProgressButton = ({ title, nextStepHandler }) => (
-  <button className="btn progress" onClick={nextStepHandler}>
+const ProgressButton = ({ title, nextStepHandler, backButton }) => (
+  <button
+    className={`btn progress${backButton ? ' back' : ''}`}
+    onClick={nextStepHandler}
+  >
+    {backButton && (
+      <img alt="move" src={backwardIcon} className="backward-icon" />
+    )}
     <span>{title}</span>
-    <img alt="move" src={forwardIcon} className="forward-icon" />
+    {!backButton && (
+      <img alt="move" src={forwardIcon} className="forward-icon" />
+    )}
   </button>
 );
 
 ProgressButton.propTypes = {
   title: PropTypes.string,
+  backButton: PropTypes.bool,
 };
 
 ProgressButton.defaultProps = {
   title: 'Button',
+  backButton: false,
 };
 
 export default ProgressButton;
