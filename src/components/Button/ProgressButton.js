07463dadf4b44959f17823b8ef987ee9ce277d88
jfan0888 <bsrg5111@gmail.com>
2019-08-16 00:05:53 +0100

    improve customisable progress button

--- src/components/Button/ProgressButton.js
+++ src/components/Button/ProgressButton.js
@@ -6,7 +6,13 @@ const forwardIcon = require('../../assets/images/forward.png');
 const backwardIcon = require('../../assets/images/backward.png');
 const shareIcon = require('../../assets/images/share.png');
 
-const ProgressButton = ({ title, nextStepHandler, backButton, sharable }) => (
+const ProgressButton = ({
+  title,
+  nextStepHandler,
+  backButton,
+  sharable,
+  noIcon,
+}) => (
   <button
     className={`btn progress${backButton ? ' back' : ''}`}
     onClick={nextStepHandler}
@@ -15,7 +21,7 @@ const ProgressButton = ({ title, nextStepHandler, backButton, sharable }) => (
       <img alt="move" src={backwardIcon} className="backward-icon" />
     )}
     <span>{title}</span>
-    {!backButton && (
+    {!backButton && !noIcon && (
       <img
         alt="move"
         src={sharable ? shareIcon : forwardIcon}
@@ -29,12 +35,14 @@ ProgressButton.propTypes = {
   title: PropTypes.string,
   backButton: PropTypes.bool,
   sharable: PropTypes.bool,
+  noIcon: PropTypes.bool,
 };
 
 ProgressButton.defaultProps = {
   title: 'Button',
   backButton: false,
   sharable: false,
+  noIcon: false,
 };
 
 export default ProgressButton;
